795e8cd0821f111b361dd7db17e64cbb9d22867f
SungpilYang <pil705@ajou.ac.kr>
2025-05-26 15:47:05 +0900

    Update AdminController.java
    finished Admin api

--- src/main/java/rph/controller/AdminController.java
+++ src/main/java/rph/controller/AdminController.java
@@ -1,18 +1,29 @@
 package rph.controller;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+
+import rph.dto.item.ItemRequest;
+import rph.dto.item.ItemResponse;
+import rph.service.item.ItemService;
 
 @RestController
 @RequestMapping("/api/admin")
 @PreAuthorize("hasRole('ADMIN')")
 public class AdminController {
 
-    @PostMapping("/item")
-    public ResponseEntity<String> addItem() {
-        // 아이템 추가 구현
-        return ResponseEntity.ok("아이템 추가 로직 위치");
+    @Autowired
+    private ItemService itemService;
+
+    @PostMapping ("/item")
+    public ResponseEntity<ItemResponse> saveItem(@Valid@RequestBody ItemRequest item){
+        ItemResponse response  = itemService.saveItem(item);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
 
